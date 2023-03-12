b8b851c75af350558cb7d270f7d5a832a78c55c6
dinaamlll <dinamaliaa17@gmail.com>
2023-03-12 11:35:23 -0800

    example

---
+++ pemdas.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//program main 
+int main() {//begin
+
+//numeric p,l,luas
+double p,l,luas;
+//display "masukkkan panjang"
+cout<<"masukkan panjang"
+//accept p
+;cin>>p;
+//display "masukkan lebar=";
+//accept l
+cin>>l;
+//compute luas=p*L
+luas=p*l
+//display "luas persegi panjang adalah", luas
+;cout<<"luas persegi panjang"
+;
+}//end
